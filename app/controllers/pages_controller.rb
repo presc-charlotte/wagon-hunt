d212bab4a4ffe77e2030097a1b9ad33e6c3f3021
presc-charlotte <topfoodfrance@gmail.com>
2016-04-26 16:47:06 +0200

    add model Product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
-class PagesController < ApplicationController
+ class PagesController < ApplicationController
   def home
+    @today = Date.today
   end
+  def team
+    @members = ["Bob", "Romain", "Seb"]
+  end
+  def join_us
+    render plain: "Send us a mail"
+  end
+
 end
