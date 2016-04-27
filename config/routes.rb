6e801324aa675d25b7a7b456b5e073c208cc5628
presc-charlotte <topfoodfrance@gmail.com>
2016-04-27 15:47:49 +0200

    add ressources routing and CRUD actions for products

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
-  get "/products/" => "products#index"
-  get "/products/:id" => "products#show"
+
+  resources :products
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
