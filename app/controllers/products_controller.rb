1f628673e62fe5a05ff5f499f781539e84119c75
presc-charlotte <topfoodfrance@gmail.com>
2016-04-28 17:34:06 +0200

    implement new and create actions on product + seed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,18 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
   def new
+    @product = Product.new
   end
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+  end
+
+
+
   end
   def edit
   end
@@ -19,4 +29,12 @@ class ProductsController < ApplicationController
   end
   def destroy
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :url)
+  end
+
+
 end
