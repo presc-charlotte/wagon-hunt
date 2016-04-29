eb9e6ee6938e5ad1766250b5c2d6ee22c7fc0121
presc-charlotte <topfoodfrance@gmail.com>
2016-04-29 17:24:33 +0200

    add category to products + implement filter by category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :url, presence: true, uniqueness: true
+  validates :category, inclusion: { in: %w(tech education design),
+    message: "%{value} is not a valid category" }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426145645) do
+ActiveRecord::Schema.define(version: 20160429140202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160426145645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "tagline"
+    t.string   "category"
   end
 
 end
